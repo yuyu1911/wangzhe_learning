b2a487cb25481d2f46fcbb10129bde59131344e9
王哲 <wangzhe@wangzhedeMacBook-Pro.local>
2015-10-28 14:03:32 +0800

    使用 select 标签 替换原来的自定义 Component

--- src/javascripts/chart/typeselector.jsx
+++ src/javascripts/chart/typeselector.jsx
@@ -3,40 +3,12 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
-class TypeSelectorItem extends React.Component {
-
-  static propTypes = {
-    value: React.PropTypes.string.isRequired,
-    typeName: React.PropTypes.string.isRequired,
-    onChangeValue: React.PropTypes.func.isRequired
-  }
-
-  selectedValue(e) {
-    e.preventDefault();
-    this.props.onChangeValue(
-      e.target.getAttribute('alt'),
-      e.target.innerText
-    );
-  }
-
-  render() {
-    return (
-      <li>
-        <a href='#'
-            onClick={ this.selectedValue.bind(this) }
-            alt={ this.props.value }>{ this.props.typeName }</a>
-      </li>
-    );
-  }
-};
-
 export default class TypeSelector extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      currentValue: this.props.items[0].typeName,
-      trigger: true
+      currentValue: this.props.items[0].value,
     }
   }
 
@@ -45,54 +17,35 @@ export default class TypeSelector extends React.Component {
     onCompleted: React.PropTypes.func.isRequired
   }
 
-  changeValue(value, typeName) {
-    this.props.onCompleted(value);
+  changeValue(evt) {
+    this.props.onCompleted(evt.target.value);
     this.setState({
-      currentValue: typeName,
-      trigger: !this.state.trigger
-    });
-  }
-  listTrigger() {
-    this.setState({
-      trigger: !this.state.trigger
-    }, this.moveToButton);
+      currentValue: evt.target.value
+    })
   }
 
   renderItem() {
     return this.props.items.map((item, i) => {
       return (
-        <TypeSelectorItem
+        <option
           key={ 'typeselectoritem-' + i }
           value={ item.value }
-          typeName={ item.typeName }
-          onChangeValue={ this.changeValue.bind(this) }
-        ></TypeSelectorItem>
+        >{ item.typeName }</option>
       );
     });
   }
 
   render() {
-    let listTriggerClassName = this.state.trigger ? 'hide' : '';
-    let showArrowClassName = this.state.trigger ? 'down' : 'up';
     return (
       <div className='selector-shell'>
-        <div className={'selector-show arrow ' + showArrowClassName } onClick={ this.listTrigger.bind(this) }>{ this.state.currentValue }</div>
-        <div className={'selector-list-shell trigger ' + listTriggerClassName} ref='listshell'>
-          <ul>
-            { this.renderItem() }
-          </ul>
-        </div>
+        <select value={ this.state.currentValue }
+                className='selector'
+                onChange={ this.changeValue.bind(this) }>
+          { this.renderItem() }
+        </select>
+        <i className='arrow'></i>
       </div>
     );
-  }
-
-  moveToButton() {
-    let elem = ReactDOM.findDOMNode(this.refs.listshell);
-    elem.style.marginBottom = -1 * parseInt(elem.offsetHeight, 10) + 'px';
-  }
-
-  componentDidMount() {
-    this.moveToButton();
   }
 
 };
